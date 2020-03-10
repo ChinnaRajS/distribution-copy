2cfef4727aafe7ec4f2f6a32dc7504f3362408df
Vidyasagar Patangi <vidyasagar.patangi@ecanarys.com>
2020-03-10 15:36:15 +0530

    Added The OrgReports Controller

---
+++ distribution-copy/Controllers/OrgReportsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace distribution_copy.Controllers
+{
+    public class OrgReportsController : Controller
+    {
+        // GET: OrgReports
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
